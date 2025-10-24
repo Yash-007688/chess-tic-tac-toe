1f064cef60089d05bbab0939f03e6d6a70fb7f86
Yash-007688 <yashrajhmoolchandani@gmail.com>
2025-10-24 18:48:32 +0530

    Remove unnecessary file

--- app_new.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, render_template, request, session, redirect, url_for  
-import random  
-import os  
-  
-app = Flask(__name__)  
-app.secret_key = os.environ.get('SECRET_KEY', 'your_secret_key_here') 
